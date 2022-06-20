6dd08f203591126b873d2d867f20a77c7b283f06
Георгий <hurricene001@mail.ru>
2022-06-20 15:58:44 +0300

    написали готовую программу

--- test/Program.cs
+++ test/Program.cs
@@ -1,3 +1,20 @@
 ﻿/*Задача 41: Написать программу, которая из имеющегося массива строк формирует
  новый массив из строк, длинна которых меньше или равна 3.*/
-Console.WriteLine("Hello, World!");
+string test = Console.ReadLine();
+string[] array = test.Split(new Char[] {' '});
+int schet=0;
+int j = 0;
+for(int i=0;i<array.Length;i++)
+{
+    if (array[i].Length<=3) schet++;
+}
+string [] result =new string [schet] ;
+for(int i=0;i<array.Length;i++)
+{
+    if (array[i].Length<=3) 
+    {
+        result[j]=array[i];
+        j++;
+    }
+}
+Console.WriteLine($"[{string.Join(", ",array)}]->[{string.Join(", ",result)}] ");
